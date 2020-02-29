7e7f82548c4d3bd2452017a6516c92fb98005133
Osmir Mariano <osmirmariano@uft.edu.br>
2020-02-29 14:30:59 -0300

    Removendo chat dessa aplicação para ser criando um outro microservico

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const serverless = require("serverless-http");
-const app = require('./src/app/')
-require('./src/app/controllers/chats/chat')
+const app = require('./src/app/');
+
 //o arquivo yml chama o serverless(app)
 module.exports.handler = serverless(app);
--- src/app/controllers/chats/room.js
+++
@@ -1,5 +0,0 @@
-class Room {
-
-}
-
-module.exports  = new Room();
